b1b656e8e6f02bfb6c9ab67b35f69ce8c7de3bad
rgseby <sebastian.raducu99@gmail.com>
2023-04-01 17:02:51 +0300

    Changed Apartament database model

--- models/Apartment.js
+++ models/Apartment.js
@@ -16,6 +16,70 @@ module.exports = (sequelize, DataTypes) => {
             validate  : {
                 notEmpty: true
             }
+        },
+
+        price_per_night : {
+            type      : DataTypes.INTEGER,
+            allowNull : false,
+            validate  : {
+                notEmpty: true
+            }
+        },
+
+        free_parking : {
+            type      : DataTypes.BOOLEAN,
+            allowNull : false,
+            valdiate  : {
+                notEmpty: true
+            }
+        },
+
+        wifi_network : {
+            type      : DataTypes.BOOLEAN,
+            allowNull : false,
+            validate  : {
+                notEmpty: true
+            }
+        },
+
+        family_room : {
+            type      : DataTypes.BOOLEAN,
+            allowNull : false,
+            validate  : {
+                notEmpty: true
+            }
+        },
+
+        smoking : {
+            type      : DataTypes.BOOLEAN,
+            allowNull : false,
+            validate  : {
+                notEmpty: true
+            }
+        },
+
+        balcony : {
+            type      : DataTypes.BOOLEAN,
+            allowNull : false,
+            validate  : {
+                notEmpty: true
+            }
+        },
+
+        heating : {
+            type      : DataTypes.BOOLEAN,
+            allowNull : false,
+            validate  : {
+                notEmpty: true
+            }
+        },
+
+        garden : {
+            type      : DataTypes.BOOLEAN,
+            allowNull : false,
+            validate  : {
+                notEmpty: true
+            }
         }
     });
 
