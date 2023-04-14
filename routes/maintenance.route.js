962a2a65dfac228ff5db4c9a2e46ba0f3c100f1f
rgseby <sebastian.raducu99@gmail.com>
2023-04-14 10:10:57 +0300

    Renamed modules

--- routes/maintenance.route.js
+++ routes/maintenance.route.js
@@ -1,7 +1,7 @@
-const express            = require('express');
-const cors               = require('cors');
+const express = require('express');
+const cors    = require('cors');
 
-const router           = express.Router()
+const router  = express.Router()
 
 
 router.post('/apartment', cors(), async (req, res) => {
