958b8da7734ae9f7d8446c5611156dc2562ffa80
rgseby <sebastian.raducu99@gmail.com>
2023-04-14 10:18:41 +0300

    Configured database

---
+++ config/db.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  HOST: "lin-18680-10731-mysql-primary.servers.linodedb.net",
+  USER: "linroot",
+  PASSWORD: "Hkwtm!wvN0WRZ7wX",
+  DB: "shiro_development",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
