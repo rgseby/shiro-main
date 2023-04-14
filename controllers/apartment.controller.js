45bd921d0f9e31d73ca0d9fce3e1880153fca6d8
rgseby <sebastian.raducu99@gmail.com>
2023-04-14 10:33:01 +0300

    Implemented simple select for aparts.

---
+++ controllers/apartment.controller.js
@@ -0,0 +1,21 @@
+const db        = require('../models');
+
+const Apartment = db.apartments;
+const Op        = db.Sequelize.Op;
+
+// Retrieve all Apartments from the database.
+exports.findAll = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  
+    Tutorial.findAll({ where: condition })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || 'Some error occurred while retrieving tutorials.'
+        });
+      });
+};
